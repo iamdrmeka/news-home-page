c00f5272894459f67384cc726115a536b7029ec5
Nnaemeka Ijeoma <nnaemekapijeoma@gmail.com>
2022-11-08 21:45:49 +0100

    done for now

--- news.js
+++ news.js
@@ -3,11 +3,13 @@
 const menuBtn = document.getElementById("menu");
 const closeBtn = document.getElementById("close");
 const menuPanel = document.getElementById("menu-panel");
+const overLay = document.querySelector(".overlay");
 
 menuBtn.addEventListener("click", function(){
     menuBtn.classList.toggle("menu-close");
     closeBtn.classList.toggle("menu-open");
     menuPanel.classList.toggle("show-mobile-menu");
+    overLay.classList.toggle("over-show");
 
 
 });
@@ -16,5 +18,18 @@ closeBtn.addEventListener("click", function(){
     menuBtn.classList.toggle("menu-close");
     closeBtn.classList.toggle("menu-open");
     menuPanel.classList.toggle("show-mobile-menu");
+    overLay.classList.toggle("over-show");
+
 
 });
+
+overLay.addEventListener('click', function(){
+    menuBtn.classList.toggle("menu-close");
+    closeBtn.classList.toggle("menu-open");
+    menuPanel.classList.toggle("show-mobile-menu");
+    overLay.classList.toggle("over-show");
+
+
+});
+
+//this code would have been written better and shorter if I have learn looping, but for now, This is howi can do it till I get better with Javascript.
